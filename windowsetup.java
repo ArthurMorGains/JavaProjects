bd2fa8517ef99d62303bd4ea8722736026948cec
ArthurMorGains <theronmccombs@gmail.com>
2025-07-30 08:37:02 -0500

    Update and rename Main.java to windowsetup.java

--- windowsetup.java
+++ windowsetup.java
@@ -1,6 +1,6 @@
 import javax.swing.JFrame;
 
-public class Main {
+public class windowsetup {
     public static void main(String[] args){
         JFrame window = new JFrame("Window");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
